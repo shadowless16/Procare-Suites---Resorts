a836bd6d88ef7c2e56b1f9f5b7dc35afcd774356
David Akinwumi <akdavid4real@gmail.com>
2025-05-22 10:55:32 +0100

    Fix database configuration: correct db_name and db_user values in config.php

--- api/config/config.php
+++ api/config/config.php
@@ -3,8 +3,8 @@
 
 return [
     'db_host' => 'localhost',      // Your MySQL host, e.g., 'localhost'
-    'db_name' => 'procare_db',     // Your database name
-    'db_user' => 'procare_user',   // Your MySQL username
+    'db_name' => 'procares_db',     // Your database name
+    'db_user' => 'procares_user',   // Your MySQL username
     'db_pass' => 'oz3Qp.!azvIN@oMP',   // Your MySQL password
 ];
 ?>
